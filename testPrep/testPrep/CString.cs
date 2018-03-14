e6da0431147e43d1d7ecaa35e1dbafa5068298f2
Xiao Liu <xliu43@nait.ca>
2018-03-14 09:53:51 -0600

    added test prep

---
+++ testPrep/testPrep/CString.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testPrep
+{
+    class CString:IComparable
+    {
+        private String str;
+
+        public CString(string s)
+        {
+            str = s;
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CString))
+            {
+                return false;
+            }
+            return str.Equals(((CString)obj).str); 
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+        public int CompareTo(object obj)
+        {
+
+
+            if (!(obj is CString))
+            {
+                throw new ArgumentException();
+            }
+            if (str.Length== ((CString)obj).str.Length)
+            {
+                return str.CompareTo(((CString)obj).str);
+            }
+            return str.Length.CompareTo(((CString)obj).str.Length);
+
+        }
+        public static int CompDesc(CString a, CString b)
+        {
+            return -1 * (a.str.CompareTo(b.str));
+
+
+        }
+
+
+        public override string ToString()
+        {
+            return str;
+        }
+    }
+}
---
+++ testPrep/testPrep/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArray = new string[8] { "a","b","C","d","a","f","gh","h"};
+            List<CString> strList = new List<CString>();
+            foreach (string s in strArray)
+            {
+                strList.Add(new CString(s));
+            }
+            Console.WriteLine("oringal List");
+            strList.ForEach(c => Console.Write(c+","));
+            Console.WriteLine();
+            
+            strList.Sort();
+            Console.WriteLine("sorted list");
+            strList.ForEach(c => Console.Write(c + ","));
+            Console.WriteLine();
+
+            strList.Sort(CString.CompDesc);
+            Console.WriteLine("sorted with Comparasion Delegtae Impelmetation");
+            strList.ForEach(c => Console.Write(c + ","));
+            Console.WriteLine();
+           
+
+            strList.Reverse();
+            Console.WriteLine("Revesred:");
+            strList.ForEach(c => Console.Write(c + ","));
+            Console.WriteLine();
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
